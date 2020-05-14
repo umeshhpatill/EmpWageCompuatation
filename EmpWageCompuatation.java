1ff2bb49cc570e5785a510ee09a97593b52e7bc3
umesh <usp5758@gmail.com>
2020-05-14 12:15:37 +0530

    employee present or absent

--- EmpWageCompuatation.java
+++ EmpWageCompuatation.java
@@ -1,6 +1,15 @@
 public class EmpWageCompuatation {
-     public static void main (String[] args) {
-       System.out.println("Welcome to Employee wage computation");
+    public static void main(String args[]){
+        //constants
+        int IS_FULL_TIME = 1;
+
+        //COMPUTATION
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == IS_FULL_TIME){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
-
