9205b993303785ca0b5e097eb9a0efe582041bcd
umesh <usp5758@gmail.com>
2020-05-14 12:22:30 +0530

    add part time Emp Wage

--- EmpWageCompuatation.java
+++ EmpWageCompuatation.java
@@ -1,18 +1,22 @@
-public classs EmpWageCompuatation {
+public class EmpWageCompuatation {
 
 public static void main(String args[]){
         //constants
         int IS_FULL_TIME = 1;
         int EMP_RATE_PER_HR = 20;
+        int IS_PART_TIME = 2;
 
         //VARIABLES
         int empHrs = 0;
         int empWage = 0;
 
         //COMPUTATION
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if(empCheck == IS_FULL_TIME){
             empHrs = 8;
+        }
+        else if(empCheck == IS_PART_TIME){
+            empHrs = 4;
         }
         else{
             empHrs = 0;
