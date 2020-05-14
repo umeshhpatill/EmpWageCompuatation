0b15bfaea85e04222e237763e94726fecaa709d6
umesh <usp5758@gmail.com>
2020-05-14 12:26:40 +0530

    Using switch case statememt

--- EmpWageCompuatation.java
+++ EmpWageCompuatation.java
@@ -1,27 +1,27 @@
 public class EmpWageCompuatation {
-
-public static void main(String args[]){
-        //constants
-        int IS_FULL_TIME = 1;
-        int EMP_RATE_PER_HR = 20;
-        int IS_PART_TIME = 2;
-
+    //constants
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HR = 20;
+    public static final int IS_PART_TIME = 2;
+    public static void main(String args[]) {
         //VARIABLES
         int empHrs = 0;
         int empWage = 0;
 
         //COMPUTATION
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if(empCheck == IS_FULL_TIME){
-            empHrs = 8;
-        }
-        else if(empCheck == IS_PART_TIME){
-            empHrs = 4;
-        }
-        else{
-            empHrs = 0;
-        }
+        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+                break;
+            }
         empWage = empHrs * EMP_RATE_PER_HR;
-        System.out.println("Employee Wage: "+empWage);
+        System.out.println("Employee Wage: " + empWage);
     }
 }
