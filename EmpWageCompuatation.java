b918e0bc1454e920eddbba578529fda5c4d2fdce
umesh <usp5758@gmail.com>
2020-05-14 12:38:10 +0530

    Refractor code write class method to employee Wage

--- EmpWageCompuatation.java
+++ EmpWageCompuatation.java
@@ -1,18 +1,20 @@
 public class EmpWageCompuatation {
 
-public static final int IS_FULL_TIME = 1;
-    public static final int EMP_RATE_PER_HR = 20;
+    public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
+
+    public static final int EMP_RATE_PER_HR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
     public static final int MAX_HRS_IN_MONTH = 100;
-    public static void main(String args[]) {
+
+    static int totalEmpWage() {
         //VARIABLES
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
         //COMPUTATION
-        while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -27,9 +29,13 @@ public static final int IS_FULL_TIME = 1;
                     break;
             }
             totalEmpHrs += empHrs;
-            System.out.println("Days: " + totalWorkingDays+" Emp Hours: "+ empHrs);
+            System.out.println("Days: " + totalWorkingDays + " Emp Hours: " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-        System.out.println("Total Emp Wage: "+totalEmpWage);
+        return totalEmpHrs * EMP_RATE_PER_HR;
+    }
+
+    public static void main(String args[]) {
+        int totalEmpWage = totalEmpWage();
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
